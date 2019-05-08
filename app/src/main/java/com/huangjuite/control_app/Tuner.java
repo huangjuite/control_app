f65ebac33cf167cb5072f7925ad17bcef12f88d2
juite <huangjuite@gmail.com>
2019-05-08 18:24:41 +0800

    update pid tuner

--- app/src/main/java/com/huangjuite/control_app/Tuner.java
+++ app/src/main/java/com/huangjuite/control_app/Tuner.java
@@ -1,6 +1,7 @@
 package com.huangjuite.control_app;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -18,6 +19,36 @@ public class Tuner {
         this.textView = textView;
         this.activity = activity;
 
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
